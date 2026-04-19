33a4bb41afe16137d30a22bc0d3688c87605adc8
alex <alexander.katholitzky@gmail.com>
2026-04-19 16:00:24 +0200

    spring boot initialization

---
+++ src/main/java/com/example/taskdesk/TaskdeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskdeskApplication.class, args);
+	}
+
+}
